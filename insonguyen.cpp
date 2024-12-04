df6a6977c692421509309e45f980d10345449c6c
letrungthanhdat2008 <letrungthanhdat6@gmail.com>
2024-12-04 17:18:17 +0700

    Add files via upload

---
+++ insonguyen.cpp
@@ -0,0 +1,16 @@
+// insonguyen.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cout << "\nNhap mot so nguyen: ";
+	cin >> num;
+
+	cout << "\nBan da nhap:" << num << endl;
+	return 0;
+}
+
